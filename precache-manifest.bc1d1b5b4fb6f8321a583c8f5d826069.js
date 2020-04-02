160b8920caa201ad3c74681524abcdfb29ae4150
Patryk Oltuch <oltix@wp.pl>
2020-04-02 11:29:34 +0200

    Updates

---
+++ precache-manifest.bc1d1b5b4fb6f8321a583c8f5d826069.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a76da9e10dc289c08ad42ad1e4da7c",
+    "url": "/syskon/index.html"
+  },
+  {
+    "revision": "fc1522fc1f8dec4cbe82",
+    "url": "/syskon/static/css/main.bde5887e.chunk.css"
+  },
+  {
+    "revision": "8b59c57b2c6489a7441d",
+    "url": "/syskon/static/js/2.4e5e23d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/syskon/static/js/2.4e5e23d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1522fc1f8dec4cbe82",
+    "url": "/syskon/static/js/main.d5aa7e59.chunk.js"
+  },
+  {
+    "revision": "64e35344046497ab73e8",
+    "url": "/syskon/static/js/runtime-main.577cab1a.js"
+  },
+  {
+    "revision": "28e1fd36a6a73f2a7e724598a8c51fdd",
+    "url": "/syskon/static/media/company.28e1fd36.png"
+  },
+  {
+    "revision": "831ff481c06d0ffa20b72ae987f902d7",
+    "url": "/syskon/static/media/img_01.831ff481.jpg"
+  },
+  {
+    "revision": "41a335b31751ae86437b81db7c4ca9bb",
+    "url": "/syskon/static/media/mainbar.41a335b3.png"
+  },
+  {
+    "revision": "be3a324bf92642780cdf61dcf7dc6081",
+    "url": "/syskon/static/media/syskon1.be3a324b.png"
+  }
+]);
